e86d59e187c4c9fc57820cb9d21040ebcf80464f
omar3ali <73795299+omar3ali@users.noreply.github.com>
2020-11-03 19:44:00 +0200

    Add smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,4 +77,16 @@ $(document).ready(function () {
 			$("#up-btn").css("display","block")
 		}else {$("#up-btn").css("display","none")}
 	})
+// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+		event.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 500, function(){
+			window.location.hash = hash;
+		});
+		}
+	});
 });
