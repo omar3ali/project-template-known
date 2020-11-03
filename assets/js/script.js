f9832b74d9e18bcb5681c9bd1665f210aac5a916
omar3ali <73795299+omar3ali@users.noreply.github.com>
2020-11-03 21:33:56 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,16 +77,5 @@ $(document).ready(function () {
 			$("#up-btn").css("opacity","1")
 		}else {$("#up-btn").css("opacity","0")}
 	})
-// Add smooth scrolling to all links
-	$("a").on('click', function(event) {
-		if (this.hash !== "") {
-		event.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(hash).offset().top
-		}, 500, function(){
-			window.location.hash = hash;
-		});
-		}
-	});
+
 });
