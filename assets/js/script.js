03800da5876033b8155d9f87e1bca953b2f764e1
omar3ali <73795299+omar3ali@users.noreply.github.com>
2020-11-03 21:47:12 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,5 +77,17 @@ $(document).ready(function () {
 			$("#up-btn").css("opacity","1")
 		}else {$("#up-btn").css("opacity","0")}
 	})
+// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+		event.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 300, function(){
+			window.location.hash = hash;
+		});
+		}
+	});
 
 });
