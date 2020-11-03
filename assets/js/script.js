ea3466bd09c81f4941c338d069a7e971dac658f3
omar3ali <73795299+omar3ali@users.noreply.github.com>
2020-11-03 23:26:23 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
 		var hash = this.hash;
 		$('html, body').animate({
 			scrollTop: $(hash).offset().top
-		}, 300, function(){
+		}, 2000, function(){
 			window.location.hash = hash;
 		});
 		}
